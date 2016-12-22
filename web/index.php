f3a7723e0a38c2768846a42783ce9466fa7242ef
Johan Hage <johan.hage@logius.nl>
2016-12-22 17:01:59 +0100

    Textual update

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/', function ()  {
-    return  "<h1>sugarmade</h1>";
+    return  "<h1>sugarmade</h1><h2>awesome</h2>";
 });
 
 $app->run();
